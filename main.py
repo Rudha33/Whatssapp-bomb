3b9eda9a32bf66489df9ae908a82d30ed63cebe0
Rudha <ksrudhai@gmail.com>
2026-07-03 23:53:13 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+#by rudha33 Next you gonna change you'r text and put massive links. 
+#change the number "the 10000" to you wanted number
+#the printed message you gonna send to you'r victim!
+
+print("ŕ̷̡̧̛̛̞̱̠͎̳̥̠̪͍̥̖̗̪̭͔̫̜̫͉̜͇̙͕̓̍̑͘͜͠ͅų̴̼̘͍̻̩̱̻̜̥̪̖͔̰̻̦̺͒͘ḋ̴̨̡̮̲̦͙̝̟̪̞̭̬͚̱̣̣̰̺̮͖̂̂͆͂̀͛͂͑̌̈͋͛̿͂̔̊̊͂̈͠h̸̨̢̯̳̝̻̺̩͍̰͙͖̯̳͇͎͔̤͖̼̠̭̩͍̭͇̲̳̞̣̘̃̓́̉͜͜ͅā̵͔͇̟̥̼̗͈͈͈̀̌̇̔̎̋̂̂̚3̴̹̰̭͕̙͚̝͇̻̠̺̮͈̲̯̱̹̪͚͉͎̪͎̥̯̳̙͉̟͋̉͛̀̉̽́̈͑̂̅̏̔̀͂͋͒͌̾͆̒̒̕͜͜3̵̨̧̢̙͙̬̯̤͍̳͙̙̮̞̙̳͉̹̱̭̯̪̝͚̥̬͚̰̳͑̌́̇̉̌̄̾̆̎̉̏̅̀̈́̎͌̈́̈̂͑͌̿̂̍̿͊́̌͌̚͘͜͝ͅ  - https://github.com/Rudha33" * 10000)
